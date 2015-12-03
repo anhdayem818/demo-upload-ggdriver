b0f7cc285146cd112cd08f364fae31bc9df12c7f
kop <thekop@ubuntu.ubuntu-domain>
2015-12-02 22:14:44 -0500

    add gitignore and define variable ENV[]

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ActiveRecord::Base
 	has_attached_file :image,
     :storage => :google_drive,
     :google_drive_credentials => "#{Rails.root}/config/google_drive.yml"
+    :styles => { :medium => "300x300" },
+    :google_drive_options => {
+      :path => proc { |style| "#{style}_#{id}_#{photo.original_filename}" }
+    }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/new'
+
+  get 'products/edit'
+
+  get 'products/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
